34c39b5ecee8abb5a84c0a76c0ee790dd067b55e
ckbullock410 <ckbullock410@gmail.com>
2018-11-21 18:39:18 -0700

    Updated files to try at schewl

---
+++ handlers.c
@@ -0,0 +1,52 @@
+
+// Header files
+#include "uart.h"
+#include "gpio.h"
+#include "irq.h"
+#include "sysreg.h"
+
+// Reference to the global mode of operation variable
+extern unsigned int mode;
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function:       IRQ_handler
+//
+//  Arguments:      none
+//
+//  Returns:        void
+//
+//  Description:    This function first prints out some basic information about
+//                  the state of the interrupt controller, GPIO pending
+//                  interrupts, and selected system registers. Determines if interrupt is
+//                  coming from pin 23 or pin 24. The interrupt is cleared, 
+//                  and interrupt is handled to transition to the correct mode.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void IRQ_handler()
+{
+    unsigned int r;   	
+
+    // Handle GPIO interrupts in general
+    if (*IRQ_PENDING_2 == 0x00100000) {
+        // Handle the interrupt associated with GPIO pin 23
+        if (*GPEDS0 == (0x1 << 23)) {         
+            // Clear the interrupt
+            *GPEDS0 = (0x1 << 23); 
+            // change the mode
+            mode = 0;
+        }
+
+        //Handle interrupt associated with GPIO pin 24
+        if (*GPEDS0 == (0x1 << 24)) {         
+            //Clear the interrupt
+            *GPEDS0 = (0x1 <<24);
+            //change the mode
+            mode = 1;
+        }
+    }    
+    // Return to the IRQ exception handler stub
+    return;
+}
